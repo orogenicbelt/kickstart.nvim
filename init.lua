1993f3b96d6f1b44e2a5f08e70f598fb7a6e0e0d
orogenicbelt <michael_carbone@protonmail.com>
2025-02-21 11:56:12 -0500

    Add [R]eveal [D]iagnostics

--- init.lua
+++ init.lua
@@ -557,6 +557,8 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader>rd', vim.diagnostic.open_float, '[R]eveal [D]iagnostics')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
